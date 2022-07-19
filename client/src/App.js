9258231334fcd8f47b0cbb7bc20bccbd360202f4
bshort985 <97572888+bshort985@users.noreply.github.com>
2022-07-18 21:48:14 -0400

    added heroku postinstall

--- client/src/App.js
+++ client/src/App.js
@@ -16,11 +16,9 @@ const httpLink = createHttpLink({
   uri: 'graphql',
 });
 
-// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+
 const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
   const token = localStorage.getItem('id_token');
-  // return the headers to the context so httpLink can read them
   return {
     headers: {
       ...headers,
@@ -30,7 +28,6 @@ const authLink = setContext((_, { headers }) => {
 });
 
 const client = new ApolloClient({
-  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
   link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
@@ -51,8 +48,8 @@ function App() {
               element={<SavedBooks />} 
             />
             <Route 
-              path="*" 
-              element={<h1 className="display-2">Wrong page!</h1>} 
+              // path="*" 
+              // element={<h1 className="display-2">Wrong page!</h1>} 
             />
           </Routes>
         </>
