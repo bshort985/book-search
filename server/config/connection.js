208a29de54d14abd27bfce71b1ddd497c8cbbc85
bshort985 <97572888+bshort985@users.noreply.github.com>
2022-07-19 18:46:54 -0400

    deployment

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/book-search',
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/project0',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
